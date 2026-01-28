9465394607d7514329519dc97c7a0c80b042b526
mahadidev7 <mahadidev7@gmail.com>
2026-01-28 22:34:41 +0600

    e fixed

--- app/page.tsx
+++ app/page.tsx
@@ -42,18 +42,6 @@ export default function Home() {
 
   return (
     <div className="relative h-full w-full bg-[#044150]">
-      <div className="p-2 text-center">
-        <select className="bg-black p-2 px-4 rounded-md" onChange={(e) => handlerSelectDictionary(e.target.value)}>
-          <option value=' '>
-              select dictionary
-            </option>
-          {data?.map((i, key) => (
-            <option value={i.data} key={key}>
-              {i.name}
-            </option>
-          ))}
-        </select>
-      </div>
       <div className="flex gap-0">
         <div className="flex-1 h-screen overflow-y-scroll ">
           <div className="flex flex-wrap justify-start">
@@ -61,7 +49,7 @@ export default function Home() {
               <div key={index} className="">
                 <p
                   onClick={() => handelDrawerFun(word)}
-                  className={`text-center text-[20px] mx-5 my-7 font-medium ${word === selectWord ? "text-[#ffd700]" : ""} pointer`}
+                  className={`text-white text-center text-[20px] mx-5 my-7 font-medium ${word === selectWord ? "text-[#ffd700]" : ""} pointer`}
                 >
                   {word},
                 </p>
@@ -69,93 +57,109 @@ export default function Home() {
             ))}
           </div>
         </div>
-        {drowar && (
-          <div className="">
-            <div className="bg-[#010b0f] h-full p-3">
-              <div className="flex flex-col gap-2 justify-between items-center">
-                <h1 className="text-xl capitalize">{selectWord}</h1>
-                <div className="flex gap-2 w-full">
-                  <input
-                    type="text"
-                    className="p-2 rounded-md border border-gray-500 w-full"
-                    placeholder="Type your meaning here..."
-                    value={searchWord}
-                    onChange={handleChange}
-                  />
-                  <button
-                    className="pointer px-3 bg-[#02293bea]"
-                    onClick={() => handelSearchFun()}
-                  >
-                    Search
-                  </button>
-                </div>
-              </div>
-              <div className="result mt-3 border-t border-gray-500 pt-3">
-                <div className="flex flex-col gap-3">
-                  <button
-                    className="pointer p-3 bg-[#05614dea] rounded-sm"
-                    onClick={() =>
-                      handelPOPUPFun(
-                        `https://dictionary.cambridge.org/dictionary/english/${selectWord}`,
-                      )
-                    }
-                  >
-                    cambridge
-                  </button>
-                  <button
-                    className="pointer p-3 bg-[#05614dea] rounded-sm"
-                    onClick={() =>
-                      handelPOPUPFun(
-                        `https://www.oxfordlearnersdictionaries.com/definition/american_english/${selectWord}?q=${selectWord}`,
-                      )
-                    }
-                  >
-                    oxford
-                  </button>
-                  <button
-                    className="pointer p-3 bg-[#05614dea] rounded-sm"
-                    onClick={() =>
-                      handelPOPUPFun(
-                        `https://translate.google.com/?sl=en&tl=bn&text=${selectWord}%0A&op=translate`,
-                      )
-                    }
-                  >
-                    google translate
-                  </button>
-                  <button
-                    className="pointer p-3 bg-[#05614dea] rounded-sm"
-                    onClick={() =>
-                      handelPOPUPFun(
-                        `https://youglish.com/pronounce/${selectWord}/english`,
-                      )
-                    }
-                  >
-                    youglish
-                  </button>
-                  <button
-                    className="pointer p-3 bg-[#05614dea] rounded-sm"
-                    onClick={() =>
-                      handelPOPUPFun(
-                        `https://ozdic.com/collocation/${selectWord}`,
-                      )
-                    }
-                  >
-                    ozdic
-                  </button>
 
-                  <button
-                    className="pointer p-3 bg-[#470561ea] rounded-sm"
-                    onClick={() => {
-                      setDrowar(!drowar);
-                    }}
-                  >
-                    Close
-                  </button>
+        <div className="d">
+          <div className="p-2 text-center">
+            <select
+              className="bg-black p-2 px-4 rounded-md"
+              onChange={(e) => handlerSelectDictionary(e.target.value)}
+            >
+              <option value=" ">select dictionary</option>
+              {data?.map((i, key) => (
+                <option value={i.data} key={key}>
+                  {i.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          {drowar && (
+            <div className="">
+              <div className="bg-[#032633] h-full p-3">
+                <div className="flex flex-col gap-2 justify-between items-center">
+                  <h1 className="text-xl capitalize text-white">{selectWord}</h1>
+                  <div className="flex gap-2 w-full">
+                    <input
+                      type="text"
+                      className="p-2 rounded-md border border-gray-500 w-full"
+                      placeholder="Type your meaning here..."
+                      value={searchWord}
+                      onChange={handleChange}
+                    />
+                    <button
+                      className="pointer text-white px-3 bg-[#02293bea]"
+                      onClick={() => handelSearchFun()}
+                    >
+                      Search
+                    </button>
+                  </div>
+                </div>
+                <div className="result mt-3 border-t border-gray-500 pt-3">
+                  <div className="flex flex-col gap-3">
+                    <button
+                      className="pointer p-3 bg-[#05614dea] rounded-sm"
+                      onClick={() =>
+                        handelPOPUPFun(
+                          `https://dictionary.cambridge.org/dictionary/english/${selectWord}`,
+                        )
+                      }
+                    >
+                      cambridge
+                    </button>
+                    <button
+                      className="pointer p-3 bg-[#05614dea] rounded-sm"
+                      onClick={() =>
+                        handelPOPUPFun(
+                          `https://www.oxfordlearnersdictionaries.com/definition/american_english/${selectWord}?q=${selectWord}`,
+                        )
+                      }
+                    >
+                      oxford
+                    </button>
+                    <button
+                      className="pointer p-3 bg-[#05614dea] rounded-sm"
+                      onClick={() =>
+                        handelPOPUPFun(
+                          `https://translate.google.com/?sl=en&tl=bn&text=${selectWord}%0A&op=translate`,
+                        )
+                      }
+                    >
+                      google translate
+                    </button>
+                    <button
+                      className="pointer p-3 bg-[#05614dea] rounded-sm"
+                      onClick={() =>
+                        handelPOPUPFun(
+                          `https://youglish.com/pronounce/${selectWord}/english`,
+                        )
+                      }
+                    >
+                      youglish
+                    </button>
+                    <button
+                      className="pointer p-3 bg-[#05614dea] rounded-sm"
+                      onClick={() =>
+                        handelPOPUPFun(
+                          `https://ozdic.com/collocation/${selectWord}`,
+                        )
+                      }
+                    >
+                      ozdic
+                    </button>
+
+                    <button
+                      className="pointer p-3 bg-[#470561ea] rounded-sm"
+                      onClick={() => {
+                        setDrowar(!drowar);
+                      }}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
